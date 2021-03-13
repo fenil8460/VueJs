911693a10441d9fc1d0e11a6475c9005e92d190d
Abhijitsinh <discussion4all@yahoo.com>
2021-03-13 12:15:02 +0530

    if and components

---
+++ hello-world/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <h1 v-if="show">Hello from Home component</h1>
+    <h1 v-else>Hello from Home component Else  </h1>
+    <!--<h2>{{data}}</h2>
+    <h2>{{msg}}</h2>-->
+    <button v-on:click="display">Toggle Element</button>
+    <button v-on:mouseover="hello">Call Hello Function</button>
+    </div>
+</template>
+<script>
+
+export default {
+    name : "Home",
+    props:{
+        data:String,
+        msg:String
+    },
+    //conditional render || if condition start
+    data()
+    {
+        return {show:true}
+    },
+     //conditional render || if condition End
+    methods: {
+        hello()
+        {
+             console.log("Hello console called")
+            alert("hello i am fenil")
+        },
+        //toggle method
+        display(){
+
+            this.show=!this.show 
+
+        }
+    },
+}
+</script>
